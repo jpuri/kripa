42b8e77320d77df0deba17610d589badca16788f
jpuri <jyotipuri@gmail.com>
2013-06-01 13:59:13 +0530

    menu changes

--- app/controllers/part_prices_controller.rb
+++ app/controllers/part_prices_controller.rb
@@ -10,6 +10,8 @@ class PartPricesController < ApplicationController
   end
 
   def ajaxSearch
+    @make = params[:make]
+    @currency = params[:currency]
     @part_prices = PartPrice.where("lower(model) like ? and lower(part_number) like ? and make = ? and currency =  ?", 
     "%#{params[:model].strip.downcase}%", "%#{params[:part_number].strip.downcase}%",
     "#{params[:make]}", "#{params[:currency]}", ).order('make asc')
