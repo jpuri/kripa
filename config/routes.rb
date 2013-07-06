a6eade9d7a0a322713e57ee17a59dadcbc289b3c
jpuri <jyotipuri@gmail.com>
2013-07-06 13:26:38 +0530

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Kripa::Application.routes.draw do
 
   get "sessions/new"
 
-  get "users/new"
-  get "users/edit"
-  get "users/update"
+#  get "users/new"
+#  get "users/edit"
+#  get "users/update"
 
   get "log_out" => "sessions#destroy", :as => "log_out"
   get "log_in" => "sessions#new", :as => "log_in"
