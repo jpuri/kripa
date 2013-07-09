67101f0b151e73ea6fe50adb8847622298bd5843
jpuri <jyotipuri@gmail.com>
2013-07-09 17:11:44 +0530

    Pushing deleted files

--- app/models/model_price.rb
+++
@@ -1,4 +0,0 @@
-class ModelPrice < ActiveRecord::Base
-  belongs_to :part_model
-  belongs_to :currency
-end
--- app/models/part_model.rb
+++
@@ -1,4 +0,0 @@
-class PartModel < ActiveRecord::Base
-  has_many :model_prices
-  belongs_to :part
-end
