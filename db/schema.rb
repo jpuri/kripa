4cb7ba22bebb387628c7b27b0260fd3387bf3a4b
Jyoti Puri <jyotipuri@gmail.com>
2013-09-01 00:41:27 +0530

    color

--- db/schema.rb
+++ db/schema.rb
@@ -12,83 +12,6 @@
 
 ActiveRecord::Schema.define(:version => 20130705171141) do
 
-  create_table "bill_parts", :force => true do |t|
-    t.integer  "bill_id"
-    t.integer  "quantity"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.float    "discount"
-    t.float    "weight"
-    t.float    "price"
-    t.integer  "part_id"
-  end
-
-  create_table "bills", :force => true do |t|
-    t.date     "bill_date"
-    t.integer  "customer_id"
-    t.integer  "currency_id"
-    t.string   "status"
-    t.float    "total_cost"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.float    "total_weight"
-    t.date     "due_date"
-    t.string   "order_number"
-    t.integer  "box_count"
-    t.string   "flight_number"
-    t.float    "previous_overdue"
-  end
-
-  create_table "currencies", :force => true do |t|
-    t.string   "name"
-    t.string   "symbol"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "customers", :force => true do |t|
-    t.string   "name"
-    t.string   "contact_person"
-    t.string   "phone"
-    t.string   "email"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "address"
-    t.string   "customer_id"
-    t.string   "country"
-    t.string   "fax"
-    t.string   "web"
-  end
-
-  create_table "makes", :force => true do |t|
-    t.string   "name"
-    t.string   "description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "model_parts", :force => true do |t|
-    t.integer  "model_id"
-    t.integer  "part_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "models", :force => true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "number"
-    t.string   "description"
-    t.integer  "make_id"
-  end
-
-  create_table "part_models", :force => true do |t|
-    t.string   "model_name"
-    t.integer  "part_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "part_prices", :force => true do |t|
     t.string   "make"
     t.string   "model"
@@ -100,23 +23,6 @@ ActiveRecord::Schema.define(:version => 20130705171141) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "color",       :default => "#FFFFFF"
-  end
-
-  create_table "parts", :force => true do |t|
-    t.float    "weight"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "description"
-    t.string   "number"
-  end
-
-  create_table "prices", :force => true do |t|
-    t.integer  "currency_id"
-    t.float    "value"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "customer_id"
-    t.integer  "part_id"
   end
 
   create_table "sessions", :force => true do |t|
