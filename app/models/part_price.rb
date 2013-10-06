d8dd5025d51d012d0597ec241eb0aca1def53501
Jyoti Puri <jyotipuri@gmail.com>
2013-10-06 20:18:31 +0530

    validations

--- app/models/part_price.rb
+++ app/models/part_price.rb
@@ -1,7 +1,6 @@
 class PartPrice < ActiveRecord::Base
   validates :make, :currency, :presence => true
-  validates_presence_of :model, :message => 'Please enter model.'
-  validates_presence_of :part_number, :message => 'Please enter part number.'
-  validates_uniqueness_of :model, case_sensitive: false , scope: [:make, :part_number, :currency] , :message => '[Make, Model, Part, Currency] combination already exists.'
-  validates_uniqueness_of :part_number, case_sensitive: false , scope: [:make, :model, :currency] , :message => '[Make, Model, Part, Currency] combination already exists.'
+  validates :model, :presence => {:message => 'Please enter model.'} 
+  validates :part_number, :presence => {:message => 'Please enter part number.'}
+  validates :model, :uniqueness => {case_sensitive: false, scope: [:make, :part_number, :currency], :message => '[Make, Model, Part, Currency] combination already exists.'}
 end
